fcf44e385758387068f3a6bdf4454cd4cd9bae99
AlexCarnelli <agcarnelli2023@gmail.com>
2024-08-17 17:39:33 -0300

    me falto el icono

--- administrador/headerADMIN.php
+++ administrador/headerADMIN.php
@@ -4,11 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="adminStyle.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+
     <title>Document</title>
 </head>
 <body>
     <header> 
     <form method="POST" action="" class="" id="">
+            <i class="fas fa-user-circle"></i>
             <button type="submit" name="pagina" value="Inicio">Inicio</button>
             <button type="submit" name="pagina" value="Control">Control</button>
             <button type="submit" name="pagina" value="Solicitudes">Solicitudes</button>
