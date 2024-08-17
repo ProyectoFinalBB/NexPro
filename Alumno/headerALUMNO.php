7e71399fdd6de9d32e67f440fb3cd50bd96ca02a
AlexCarnelli <agcarnelli2023@gmail.com>
2024-08-17 17:47:03 -0300

    Primeros pasos interfaz profesor

--- Alumno/headerALUMNO.php
+++ Alumno/headerALUMNO.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="alumnoStyle.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
-    <title>Document</title>
+    <title>Alumno</title>
 </head>
 <body>
     <header> 
