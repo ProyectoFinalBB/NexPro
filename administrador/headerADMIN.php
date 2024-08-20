4a36bd90968328b71e6e71b63aecfd5edd72deb9
Alex <agcarnelli2023@gmail.comm>
2024-08-20 12:00:30 -0300

    Cambio de titulos

--- administrador/headerADMIN.php
+++ administrador/headerADMIN.php
@@ -5,16 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="adminStyle.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-
-=======
->>>>>>> NEX-3-Interfaz-Alumno
-    <title>Document</title>
-=======
     <title>Administrador</title>
->>>>>>> NEX-4-Interfaz-Profesor
+
 </head>
 <body>
     <header> 
