5747b4f4af77d1e7e85dbd620a685fcec5c4fb1c
AlexCarnelli <agcarnelli2023@gmail.com>
2024-09-30 11:03:41 -0300

    Pequeños arreglos: Alex

--- includes/footer.php
+++ includes/footer.php
@@ -2,9 +2,9 @@
     <div class="footer-content">
         <span class="footer-text">© 2024 DESARROLLADORES CLAJ</span>
         <div class="social-icons">
-            <a href="https://www.instagram.com/its_utu.paysandu/" class="social-icon"><i class="fab fa-instagram"></i></a>
-            <a href="https://www.facebook.com/itsutupaysandu?locale=es_LA" class="social-icon"><i class="fab fa-facebook-f"></i></a>
-            <a href="https://uy.linkedin.com/school/utu-uruguay/" class="social-icon"><i class="fab fa-linkedin-in"></i></a>
+            <a href="https://www.instagram.com/its_utu.paysandu/" class="social-icon" target="_blank"><i class="fab fa-instagram"></i></a>
+            <a href="https://www.facebook.com/itsutupaysandu?locale=es_LA" class="social-icon" target="_blank"><i class="fab fa-facebook-f"></i></a>
+            <a href="https://uy.linkedin.com/school/utu-uruguay/" class="social-icon" target="_blank"><i class="fab fa-linkedin-in"></i></a>
         </div>
     </div>
 </footer>
