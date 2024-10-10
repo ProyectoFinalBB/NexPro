7444fae4fb5f341118b4b4903c551d9a5377d7bf
AlexCarnelli <agcarnelli2023@gmail.com>
2024-10-10 09:35:03 -0300

    codiigo para la tabla proyectos cambiado

--- tablasBD/proyectos.sql
+++ tablasBD/proyectos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-09-2024 a las 22:11:40
+-- Tiempo de generación: 10-10-2024 a las 14:33:45
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -35,13 +35,22 @@ CREATE TABLE `proyectos` (
   `id_integrantes` text NOT NULL,
   `tags` varchar(255) NOT NULL,
   `fecha_subida` timestamp NOT NULL DEFAULT current_timestamp(),
-  `estado` enum('pendiente','aceptado','denegado') DEFAULT 'pendiente'
+  `estado` enum('pendiente','aceptado','denegado') DEFAULT 'pendiente',
+  `id_usr_creador` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `proyectos`
 --
 
+INSERT INTO `proyectos` (`id`, `titulo`, `descripcion`, `ruta`, `id_integrantes`, `tags`, `fecha_subida`, `estado`, `id_usr_creador`) VALUES
+(38, 'NexPro', 'Software para proyectos', 'Practico CU.pdf', '32', 'Educación', '2024-10-07 18:26:46', 'aceptado', NULL),
+(39, 'aa', 'aa', 'Practico CU.pdf', '32', 'Entretenimiento', '2024-10-07 18:56:01', 'aceptado', NULL),
+(40, 'HAHJHa', 'hjGJ', 'Escrito Formación Empresarial.pdf', '33', 'Finanzas,Ciber seguridad', '2024-10-08 14:12:14', 'aceptado', NULL),
+(41, 'aa', 'aa', 'Escrito Formación Empresarial.pdf', '33', 'Entretenimiento', '2024-10-08 15:05:00', 'aceptado', 32),
+(42, 'fede', 'ingles', 'Escrito Formación Empresarial.pdf', '33', 'Educación', '2024-10-09 15:46:10', 'denegado', 23),
+(43, 'fede', 'ingles', 'Escrito Formación Empresarial.pdf', '33', 'Educación', '2024-10-09 15:46:15', 'aceptado', 23),
+(44, 'sssssssssss', 'ssssssss', 'Practico CU.pdf', '33', 'Entretenimiento', '2024-10-10 10:57:04', 'pendiente', 23);
 
 --
 -- Índices para tablas volcadas
@@ -61,7 +70,7 @@ ALTER TABLE `proyectos`
 -- AUTO_INCREMENT de la tabla `proyectos`
 --
 ALTER TABLE `proyectos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
