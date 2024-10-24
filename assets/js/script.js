f7bcf6a1bbb1c108e14859eb717148d9c96870f4
NexPro <proyectofinalbb@gmail.com>
2024-10-24 09:58:55 -0300

    1 tarea finalizada(menues)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,64 @@
-
-   document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
+    // Configuración inicial
     document.getElementById('header').style.display = 'flex';
     document.getElementById('menuCTRLUsuario').style.display = 'none';
     document.getElementById('menuSolicitudProyectos').style.display = 'none';
-    document.getElementById('userData').style.display = 'none'; 
-    Listado('../controllers/listarEstudiante.php')
-    ListadoProyectosPendientes()
-    ListadoProyectosAceptados()
+    document.getElementById('userData').style.display = 'none';
+    
+    // Inicializar los listados
+    Listado('../controllers/listarEstudiante.php');
+    ListadoProyectosPendientes();
+    ListadoProyectosAceptados();
 
-
+    
+        // Cerrar modales al hacer clic fuera de ellos
+        window.onclick = function(event) {
+            const modalProyectoInicio = document.getElementById('modalProyectoInicio');
+            const modalProyecto = document.getElementById('modalProyecto');
+    
+            if (event.target == modalProyectoInicio) {
+                cerrarModalInicio();
+            }
+            if (event.target == modalProyecto) {
+                cerrarModal();
+            }
+        };
+    
+    // Cerrar el menú cuando se haga clic fuera de él
+    document.addEventListener('click', function(event) {
+        const menuPerfil = document.getElementById('menuPerfil');
+        const navBtn = document.getElementById('nav-btn');
+        
+        // Verificar si el clic ocurrió fuera del menú y del botón de navegación
+        if (!menuPerfil.contains(event.target) && !navBtn.contains(event.target)) {
+            cerrarMenu(); // Cerrar el menú si se hace clic fuera de él
+        }
+    });
 });
+
+// Función para abrir el menú
+function abrirMenu() {
+    const menuPerfil = document.getElementById('menuPerfil');
+    if (esMovil()) {
+        menuPerfil.classList.add('menu-abierto-movil');
+        menuPerfil.classList.remove('menu-cerrado-movil');
+    } else {
+        menuPerfil.classList.add('menu-abierto-pc');
+        menuPerfil.classList.remove('menu-cerrado-pc');
+    }
+}
+
+// Función para cerrar el menú
+function cerrarMenu() {
+    const menuPerfil = document.getElementById('menuPerfil');
+    if (esMovil()) {
+        menuPerfil.classList.add('menu-cerrado-movil');
+        menuPerfil.classList.remove('menu-abierto-movil');
+    } else {
+        menuPerfil.classList.add('menu-cerrado-pc');
+        menuPerfil.classList.remove('menu-abierto-pc');
+    }
+}
 
 function redirectToView(ruta, param) {
     const baseURL = '../controllers/viewController.php?param='+param;
