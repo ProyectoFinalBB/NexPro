44a3f5cb2b20c8720629e93fdab063c3c7fc7a80
AlexCarnelli <agcarnelli2023@gmail.com>
2024-10-25 17:49:21 -0300

    se valida la cedula al modificar usuario

--- controllers/modificarUsuario.php
+++ controllers/modificarUsuario.php
@@ -1,12 +1,13 @@
 <?php
 session_start();
-if (!isset($_SESSION['ci']) && $_SESSION["rol"]!=="administrador") {
+if (!isset($_SESSION['ci']) && $_SESSION["rol"] !== "administrador") {
     header('Location: ../public/login.php'); 
     exit();
 }
 
 $data = json_decode(file_get_contents('php://input'), true);
 include('../includes/conexion.php');
+include('validarCI.php'); 
 $conn = conectar_bd();
 
 if (isset($data['nombre'], $data['apellido'], $data['ci'], $data['rol'])) {
@@ -19,23 +20,29 @@ if (isset($data['nombre'], $data['apellido'], $data['ci'], $data['rol'])) {
 
     $nombreCompleto = "$nombre $apellido";
 
-    $sql_usuarios = "UPDATE usuarios SET nombrecompleto = '$nombreCompleto', ci = '$ci' WHERE id_usr = $id_usr";
-    $resultado_usuarios = mysqli_query($conn, $sql_usuarios);
+    
+    if (validarCI($ci)) {
+        $sql_usuarios = "UPDATE usuarios SET nombrecompleto = '$nombreCompleto', ci = '$ci' WHERE id_usr = $id_usr";
+        $resultado_usuarios = mysqli_query($conn, $sql_usuarios);
 
-    if ($resultado_usuarios) {
-        $sql_roles = "UPDATE roles SET rol = '$rol' WHERE id_usr = $id_usr";
-        $resultado_roles = mysqli_query($conn, $sql_roles);
+        if ($resultado_usuarios) {
+            $sql_roles = "UPDATE roles SET rol = '$rol' WHERE id_usr = $id_usr";
+            $resultado_roles = mysqli_query($conn, $sql_roles);
 
-        if ($resultado_roles) {
-            echo json_encode(['Rol actualizado']);
+            if ($resultado_roles) {
+                echo json_encode('Rol actualizado');
+            } else {
+                echo json_encode('Error al actualizar el rol.');
+            }
         } else {
-            echo json_encode(['Error al actualizar el rol.']);
+            echo json_encode('Error al actualizar los datos del usuario.');
         }
     } else {
-        echo json_encode(['Error al actualizar los datos del usuario.']);
+        echo json_encode('Cedula invalida');
     }
 
     mysqli_close($conn);
 } else {
-    echo json_encode(['Datos incompletos.']);
+    echo json_encode('Datos incompletos.');
 }
+?>
